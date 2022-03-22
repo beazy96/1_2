5f8ebfc4e74a186c31948a62d0117f959c273ebc
beazy96 <96582724+beazy96@users.noreply.github.com>
2022-03-22 11:40:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+        
+        int x = 9;
+        byte y = 1;
+        long z = 2;
+        float u = 2;
+        
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(u);
+  }
+}
